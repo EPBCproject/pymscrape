cc2bccba95fad6f54b89ba3345775776a13aac6a
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2021-08-26 13:38:25 +1000

    fixed byg with filenames with underscores

--- menu.py
+++ menu.py
@@ -230,7 +230,6 @@ class Menu(ttk.Frame):
         return
 
     def check_saved_pages(self, event=None):
-        import pdb; pdb.set_trace()
         id_num = self.file_path.split('/')[-1].split('.')[:-1]
         id_num = ''.join(id_num)
         id_num = id_num.replace('-', ' ').replace('_', ' ')
